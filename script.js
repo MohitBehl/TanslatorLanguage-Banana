708b1d795c58d167f2efe7193520c1993e214346
MohitBehl <mohitbehlofficial@gmail.com>
2021-12-09 12:06:48 +0530

    done with translator

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputTextArea = document.querySelector("#textArea");
 var outputDivArea = document.querySelector("#output");
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedUrl(text) {
+  return serverUrl + "?text=" + text;
+}
 
 function onClickHandler() {
-  if (inputTextArea.value.length === 0) {
-    outputDivArea.innerText = "enter text to translate";
-  } else {
-    outputDivArea.innerText = inputTextArea.value;
-  }
+  alert(inputTextArea.value);
+  fetch(getTranslatedUrl(inputTextArea.value))
+    .then(function responseHandler(response) {
+      return response.json();
+    })
+    .then(function logJson(json) {
+      outputDivArea.innerText = json.contents.translated;
+    })
+    .catch(errorHandler);
+}
+
+function errorHandler(error) {
+  outputDivArea.innerText = "caught some server error, error code:" + error;
 }
 
 btnTranslate.addEventListener("click", onClickHandler);
